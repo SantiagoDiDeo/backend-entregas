18ecadd2a803215292bfde784e6dde3a021901f1
Santiago Di Deo <santiagodideo@gmail.com>
2023-02-06 20:24:34 -0300

    entrega diez v1

---
+++ desafioEntregableDiez/public/index.js
@@ -0,0 +1,99 @@
+const socket = io();
+
+socket.on('connect', () => {
+    console.log('connected')
+});
+//agregar nuevo producto
+const formulario = document.getElementById('form')
+
+const formAddE = async () => {
+
+    let formulario = await formulario.addEventListener('submit',  e => {
+        e.preventDefault()
+        const product = {
+            "title": title.value,
+            "price": price.value,
+            "thumbnail": thumbnail.value
+        }
+         socket.emit('newProduct', product)
+        
+        })
+
+}
+
+
+
+
+//lista productos
+socket.on('products',  (data) => {
+    products = data;
+    let htmlToRender = `
+  <table class="table container">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Precio</th>
+        <th scope="col">Foto</th>
+      </tr>
+    </thead>
+    </tbody>`;
+   
+    products.forEach(( element) => {
+        htmlToRender = htmlToRender + `
+        <tr>
+          <th scope="row">${element.id}</th>
+          <td>${element.title}</td>
+          <td>${element.price}</td>
+          <td><img src=${element.thumbnail} style="max-width: 50px; height: auto;"</td>
+        </tr>` 
+    
+    })
+    htmlToRender = htmlToRender + '</tbody></table>';
+    document.getElementById('tableProducts').innerHTML = htmlToRender;
+})
+
+
+
+
+//chat
+socket.on('chat', async (data) => {
+    chat = data;    
+    htmlToRender = '';
+    await chat.forEach((element) => {
+
+        htmlToRender = htmlToRender + `
+        <tr>
+            <th><h1 class='user'>${element.email}</h1></th>
+            <th><h1 class='mensaje'>${element.message}</h1></th>
+            <th><h1 class='date'>${element.date}</h1></th>
+        </tr>
+        `
+    })
+
+    document.getElementById('message').innerHTML = htmlToRender;
+    })
+    
+
+function validateEmail(email) {
+    const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if(email.match(mailformat)) {
+      return true
+    } else {
+      alert("You have entered an invalid email address!");
+      return false
+    }
+  }
+
+let addMessage = (addMessage) => {
+let messageToAdd = {
+    email: email.value,
+    message: mensaje.value,
+    date: new Date().toLocaleDateString()
+}
+
+if(validateEmail(email.value)) {
+
+    socket.emit('newMessage', messageToAdd)
+}
+}
---
+++ desafioEntregableDiez/routes/prodRouter.js
@@ -0,0 +1,64 @@
+const express = require( 'express' );
+const {products} = require('../class/prodClass')
+const { faker } = require('@faker-js/faker');
+const { mockProducts } = require('../class/mockClass');
+faker.locate = 'es';
+const { Router } = express;
+
+const prodRouter = Router();
+
+//get all products
+prodRouter.get('/', async ( req, res ) => {
+    const allProducts = await products.getArray();
+    res.json(allProducts);
+});
+
+//get products by id
+prodRouter.get('/:id', async ( req, res ) => {
+    let id = req.params.id;
+    const product = await products.getById(id)
+    product ? res.json( product )
+    : res.status(404).send({ error: 'producto no encontrado'})  
+
+});
+
+//post product
+prodRouter.post('/productos', async (req, res) => {
+    const productToAdd = await req.body
+    await products.addProduct(productToAdd)
+    res.redirect('/')
+  })
+
+//update product
+prodRouter.put('/:id', async ( req, res ) => {
+    const id = req.params.id;
+    const  replace  = req.body;
+
+    const index = id - 1;
+
+    if(await products.adjustById( id, productToModify )){
+        res.send({ message: 'producto modificado'})
+      } else {
+        res.status(404).send({ error: 'producto no encontrado'})
+      }
+    
+});
+
+//delete product
+prodRouter.delete('/:id', async ( req, res ) => {
+    const { id } = req.params;
+
+    if (await products.delById(id)) {
+        res.send({ message: 'producto borrado'})
+      } else {
+        res.status(404).send({ error: 'producto no encontrado'})
+      }
+});
+
+prodRouter.get('/productos-test', (req, res) => {
+  const products = mockProducts.getArray();
+
+  res.json(products);
+})
+
+module.exports = prodRouter;
